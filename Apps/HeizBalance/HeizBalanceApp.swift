38b9933a72fd526ceb21ac74b41f24464c2ed59e
Kamilunavo <acciento89@gmail.com>
2026-08-25 10:32:01 +0200

    feat(heizbalance): wire project store into app environment

--- Apps/HeizBalance/HeizBalanceApp.swift
+++ Apps/HeizBalance/HeizBalanceApp.swift
@@ -2,9 +2,12 @@ import SwiftUI
 
 @main
 struct HeizBalanceApp: App {
+    @State private var projectStore = HeizBalanceProjectStore()
+
     var body: some Scene {
         WindowGroup {
             HeizBalanceView()
+                .environment(projectStore)
                 .preferredColorScheme(.dark)
                 .shkKeyboardDismissal()
         }
